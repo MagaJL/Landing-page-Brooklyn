68e915e28c9e3a63090dc095aef39c51762d2c11
Tatibnomb <49379010@est.ort.edu.ar>
2026-08-20 15:49:11 -0300

    Agregué que se deslicen las cards de los productos, y puse comentarios del código

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-/* NAVBAR */
+// Navbar
 
 const navbar = document.getElementById("navbar");
 
+// Detecta cuando el usuario hace scroll en la página
 window.addEventListener("scroll", () => {
-
+// Si el usuario bajó más de 50 píxeles...
     if (window.scrollY > 50) {
+        // agrega la clase "scrolled" a la navbar
         navbar.classList.add("scrolled");
     } else {
+        // Si vuelve arriba, sacamos la clase
         navbar.classList.remove("scrolled");
     }
 
 });
 
 
-/* MENU */
+// Menú
 
 const menuButton =
     document.getElementById("menu-button");
@@ -28,7 +31,7 @@ menuButton.addEventListener("click", () => {
 });
 
 
-/* CERRAR MENU */
+// Cerrar menú
 
 document.querySelectorAll(".nav-menu a").forEach(link => {
 
@@ -41,19 +44,23 @@ document.querySelectorAll(".nav-menu a").forEach(link => {
 });
 
 
-/* BOTONES */
+// Botones
 
+// Selecciona todos los botones de compra
 const buyButtons =
     document.querySelectorAll(".buy-button");
 
+// Recorre todos los botones
 buyButtons.forEach(button => {
-
+    // Detecta cuando se hace clic en un botón
     button.addEventListener("click", () => {
 
+        // Cambia temporalmente el símbolo "+" por un ✓ para indicar que se seleccionó
         button.textContent = "✓";
-
+        // Cambia el color del botón
         button.style.background = "#BD7028";
 
+        // Después de 1,5 segundos vuelve al estado original
         setTimeout(() => {
 
             button.textContent = "+";
@@ -65,7 +72,7 @@ buyButtons.forEach(button => {
 
 });
 
-/* Formulario de pedido */
+// Formulario de pedido
 
 const orderButton =
     document.getElementById("order-button");
@@ -80,7 +87,7 @@ const orderForm =
     document.getElementById("order-form");
 
 
-/* Abrir formulario */
+// Abrir formulario
 
 orderButton.addEventListener("click", () => {
 
@@ -89,7 +96,7 @@ orderButton.addEventListener("click", () => {
 });
 
 
-/* Cerrar formulario */
+// Cerrar formulario
 
 closeOrder.addEventListener("click", () => {
 
@@ -98,7 +105,7 @@ closeOrder.addEventListener("click", () => {
 });
 
 
-/* Cerrar al hacer click fuera del formulario */
+// Cerrar al hacer click fuera del formulario
 
 orderModal.addEventListener("click", (event) => {
 
@@ -111,7 +118,7 @@ orderModal.addEventListener("click", (event) => {
 });
 
 
-/* Enviar pedido */
+// Enviar pedido
 
 orderForm.addEventListener("submit", (event) => {
 
@@ -122,5 +129,53 @@ orderForm.addEventListener("submit", (event) => {
     orderForm.reset();
 
     orderModal.classList.remove("active");
+
+});
+
+// Deslizar los productos (brookies)
+const cards = document.querySelectorAll(".product-card");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+
+function updateCarousel() {
+
+    cards.forEach(card => {
+        card.classList.remove("active");
+    });
+
+    cards[current].classList.add("active");
+
+}
+
+
+// Siguiente
+
+next.addEventListener("click", () => {
+
+    current++;
+
+    if (current >= cards.length) {
+        current = 0;
+    }
+
+    updateCarousel();
+
+});
+
+
+// Anterior
+
+prev.addEventListener("click", () => {
+
+    current--;
+
+    if (current < 0) {
+        current = cards.length - 1;
+    }
+
+    updateCarousel();
 
 });
